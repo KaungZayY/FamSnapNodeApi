43fc2fcdfbf94707ec14986aa750f8d5a7a515d0
KaungZayY <kaungzayyan19@gmail.com>
2024-08-12 18:57:48 +0700

    GET request -> /albums to display all albums

--- server.js
+++ server.js
@@ -4,7 +4,33 @@ import url from 'url';
 const PORT = process.env.PORT;
 
 const server = http.createServer(async(req,res)=>{
-    
+    res.setHeader('Content-Type', 'application/json');
+    try
+    {
+        if(req.method === 'GET')
+        {
+            //get all albums
+            if(req.url === '/albums')
+            {
+                try 
+                {
+                    const results = await qry('SELECT * FROM albums');
+                    res.statusCode = 200;
+                    res.end(JSON.stringify(results));
+                } 
+                catch (err) 
+                {
+                    res.statusCode = 500;
+                    res.end(JSON.stringify({error: `${err}`}));
+                }
+            }
+        }
+    }
+    catch(error)
+    {
+        res.statusCode = 500
+        res.end(JSON.stringify({ error: `Server Error: ${error}` }));
+    }
 })
 
 server.listen(PORT,()=>{
