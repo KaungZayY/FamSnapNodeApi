0e6b166a244d2f7aa69569a7f22c8de557ea41a3
KaungZayY <kaungzayyan19@gmail.com>
2024-08-13 14:28:06 +0700

    get album by id

--- server.js
+++ server.js
@@ -3,34 +3,68 @@ import http from 'http';
 import url from 'url';
 const PORT = process.env.PORT;
 
-const server = http.createServer(async(req,res)=>{
-    res.setHeader('Content-Type', 'application/json');
-    try
+const middleWare = (req,res,next) => {
+    //middle ware contents here
+    next();
+}
+
+const getAlbums = async (res) =>{
+    try 
     {
-        if(req.method === 'GET')
-        {
-            //get all albums
-            if(req.url === '/albums')
-            {
-                try 
-                {
-                    const results = await qry('SELECT * FROM albums');
-                    res.statusCode = 200;
-                    res.end(JSON.stringify(results));
-                } 
-                catch (err) 
-                {
-                    res.statusCode = 500;
-                    res.end(JSON.stringify({error: `${err}`}));
-                }
-            }
+        const results = await qry('SELECT * FROM albums');
+        res.statusCode = 200;
+        res.end(JSON.stringify(results));
+    } 
+    catch (err) 
+    {
+        res.statusCode = 500;
+        res.end(JSON.stringify({error: `${err}`}));
+    }
+}
+
+const getAlbumById = async (req,res)=>{
+    const id = req.url.split('/')[3];
+    try 
+    {
+        const results = await qry('SELECT * FROM albums WHERE id=?',[id]);
+        if (results.length > 0) {
+            res.statusCode = 200;
+            res.end(JSON.stringify(results));
+        } else {
+            res.statusCode = 404;
+            res.end(JSON.stringify({ message: 'Album Not Found' }));
         }
-    }
-    catch(error)
+    } 
+    catch (err) 
     {
-        res.statusCode = 500
-        res.end(JSON.stringify({ error: `Server Error: ${error}` }));
+        res.statusCode = 500;
+        res.end(JSON.stringify({error: `${err}`}));
     }
+}
+
+const notFound = (res) => {
+    res.statusCode = 404;
+    res.end(JSON.stringify({error:'Not Found'}))
+}
+
+const server = http.createServer((req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+
+    middleWare(req,res,()=>{
+        // GET: /api/albums
+        if(req.url === '/api/albums' && req.method === 'GET')
+        {
+            getAlbums(res);
+        }
+        // GET: /api/albums/:id
+        else if(req.url.match(/\/api\/albums\/([0-9]+)/) && req.method === 'GET'){
+            getAlbumById(req,res);
+        }
+        else
+        {
+            notFound(res);
+        }
+    })
 })
 
 server.listen(PORT,()=>{
