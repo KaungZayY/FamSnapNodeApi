ab3d3b5347fae5ea48ffe95eefcce19bb38641dd
KaungZayY <kaungzayyan19@gmail.com>
2024-08-18 10:30:15 +0700

    DB PORT

--- database.js
+++ database.js
@@ -8,6 +8,7 @@ const pool = createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     connectionLimit: process.env.DB_CONNECTIONLIMIT
 });
 
